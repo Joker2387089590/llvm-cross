e89b21f873849ce2ceaa6a5478dbe79203dcf4db
joker.lin <joker.lin@greentest.com.cn>
2022-05-09 18:28:53 +0800

    add link-fs

--- build-libcxx.sh
+++ build-libcxx.sh
@@ -1,24 +1,23 @@
-export SYSROOT=/home/joker/repo/llvm/install
-export PATH=$PATH:${SYSROOT}
+export SYSROOT=/home/joker/repo/llvm/install-2
 
 cmake ~/repo/llvm/llvm-project/runtimes \
 -G Ninja \
--D CMAKE_PREFIX_PATH=${SYSROOT} \
--D CMAKE_INSTALL_PREFIX=${SYSROOT} \
 -D CMAKE_SYSROOT=${SYSROOT} \
--D CMAKE_FIND_ROOT_PATH=${SYSROOT} \
--D CMAKE_TOOLCHAIN_FILE=~/repo/llvm/cmake/libcxx.cmake \
+-D CMAKE_INSTALL_PREFIX=${SYSROOT}/usr/local \
+-D CMAKE_TOOLCHAIN_FILE=/home/joker/repo/llvm/cmake/libcxx.cmake \
 -D LLVM_ENABLE_RUNTIMES="libunwind;libcxxabi;libcxx" \
 -D LIBUNWIND_USE_COMPILER_RT=ON \
 -D LIBCXXABI_USE_LLVM_UNWINDER=ON \
 -D LIBCXXABI_USE_COMPILER_RT=ON \
 -D LIBCXXABI_ENABLE_THREADS=ON \
--D LIBCXXABI_HAS_CXA_THREAD_ATEXIT_IMPL=FALSE \
--D LIBCXX_HAS_ATOMIC_LIB=FALSE \
 -D LIBCXX_CXX_ABI=libcxxabi \
--D LIBCXX_HAS_MUSL_LIBC=TRUE \
 -D LIBCXX_USE_COMPILER_RT=ON \
 -D CMAKE_SIZEOF_VOID_P=4
+
+# -D CMAKE_FIND_ROOT_PATH=${SYSROOT} \
+# -D LIBCXXABI_HAS_CXA_THREAD_ATEXIT_IMPL=FALSE \
+# -D LIBCXX_HAS_ATOMIC_LIB=FALSE \
+# -D LIBCXX_HAS_MUSL_LIBC=TRUE \
 
 # Bugs made by cmake 'check_library_exists':
 #   When cmake runs try_compile, it uses the libraries on host path.
---
+++ link-fs.sh
@@ -0,0 +1,8 @@
+export SYSROOT=/mnt/sglib
+mkdir lib
+mkdir -p usr/local
+
+ln -s $SYSROOT/include include
+ln -s $SYSROOT/lib lib
+ln -s $SYSROOT/usr/include usr/include
+ln -s $SYSROOT/usr/lib usr/lib
