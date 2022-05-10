2080ffaedc7ce5bced01947b891f2f61792b1736
joker.lin <joker.lin@greentest.com.cn>
2022-05-10 18:33:18 +0800

    add musl-arm-toolchain.cmake

---
+++ cmake/musl-arm-toolchain.cmake
@@ -0,0 +1,47 @@
+set(CMAKE_SYSTEM_NAME Linux) # CMAKE_CROSSCOMPILING is set to ON automatically by this.
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY) # magic option...
+
+# set sysroot
+set(CMAKE_SYSROOT "/home/joker/repo/llvm/install")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# set compiler as Clang
+set(CMAKE_ASM_COMPILER /bin/clang)
+set(CMAKE_C_COMPILER   /bin/clang)
+set(CMAKE_CXX_COMPILER /bin/clang++)
+
+# compiler target triple
+set(CLANG_TARGET_TRIPLE "armv7a-unknown-linux-musleabihf")
+set(CMAKE_ASM_COMPILER_TARGET ${CLANG_TARGET_TRIPLE})
+set(CMAKE_C_COMPILER_TARGET   ${CLANG_TARGET_TRIPLE})
+set(CMAKE_CXX_COMPILER_TARGET ${CLANG_TARGET_TRIPLE})
+
+# use the custom built libc++
+set(CLANG_COMPILER_FLAGS 
+    "-mcpu=cortex-a9 -mfpu=vfpv3-d16 -mfloat-abi=hard \
+     -nostdlib++ -isystem ${CMAKE_SYSROOT}/include/c++/v1")
+set(CLANG_LINKER_FLAGS 
+    "-fuse-ld=lld -rtlib=compiler-rt -unwindlib=libunwind \
+     -L ${CMAKE_SYSROOT}/lib -Wl,-rpath,${CMAKE_SYSROOT}/lib -lc++")
+
+# let CMake use -std=c++** instead of -std=gnu++**
+set(CMAKE_C_EXTENSIONS OFF)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${CLANG_COMPILER_FLAGS}")
+set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   ${CLANG_COMPILER_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CLANG_COMPILER_FLAGS}")
+
+set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS}    ${CLANG_LINKER_FLAGS}")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${CLANG_LINKER_FLAGS}")
+# set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} ${CLANG_LINKER_FLAGS}")
+
+if(MUSL_ARM_CHAINLOAD_TOOLCHAIN_FILE)
+    include(${MUSL_ARM_CHAINLOAD_TOOLCHAIN_FILE})
+endif()
